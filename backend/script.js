95d59487959561997f7881ee4738ed682f7d3cc1
Lyla Jane Villanueva <lylajane.villanueva@tup.edu.ph>
2025-12-06 19:07:10 +0800

    unting changes sa homepage

--- backend/script.js
+++ backend/script.js
@@ -1,7 +1,7 @@
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("navMenu");
 const icon = hamburger.querySelector("i");
-
+const ref = carousel();
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("show");
 
@@ -19,3 +19,35 @@ document.querySelectorAll("#navMenu a").forEach(link => {
         icon.classList.replace("fa-xmark", "fa-bars");
     });
 });
+
+function carousel() {
+    const track = document.querySelector(".carousel-track");
+    const items = document.querySelectorAll(".carousel-item");
+    const prevBtn = document.querySelector(".prev");
+    const nextBtn = document.querySelector(".next");
+
+    let index = 0;
+
+    function update() {
+    const width = document.querySelector(".carousel").offsetWidth;
+    track.style.transform = `translateX(-${index * width}px)`;
+    }
+
+    nextBtn.addEventListener("click", () => {
+    index = (index + 1) % items.length;
+    update();
+    });
+
+    prevBtn.addEventListener("click", () => {
+    index = (index - 1 + items.length) % items.length;
+    update();
+    });
+
+    window.addEventListener("resize", update);
+
+    setInterval(() => {
+    index = (index + 1) % items.length;
+    update();
+    }, 3000);
+}
+
