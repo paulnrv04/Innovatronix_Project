50025230287de7ceae8ab8874a8006be24e47262
Lyla Jane Villanueva <lylajane.villanueva@tup.edu.ph>
2025-12-07 17:33:51 +0800

    homepage with nightmode(seperated na)

--- backend/script.js
+++ backend/script.js
@@ -2,7 +2,6 @@ const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("navMenu");
 const icon = hamburger.querySelector("i");
 const ref = carousel();
-const darkMode = darkModeToggle();
 
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("show");
@@ -51,22 +50,4 @@ function carousel() {
     index = (index + 1) % items.length;
     update();
     }, 3000);
-}
-
-function darkModeToggle() {
-  const toggleBtn = document.getElementById('themeToggle');
-  const body = document.body;
-
-  // optional: remember choice
-  const savedTheme = localStorage.getItem('theme');
-  if (savedTheme === 'dark') {
-    body.classList.add('dark-mode');
-    toggleBtn.textContent = '☀️';
-  }
-
-  toggleBtn.addEventListener('click', () => {
-    const isDark = body.classList.toggle('dark-mode');
-    toggleBtn.textContent = isDark ? '☀️' : '🌙';
-    localStorage.setItem('theme', isDark ? 'dark' : 'light');
-  });
 }
